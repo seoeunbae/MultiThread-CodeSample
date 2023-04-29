dc2e7520777cfbc1f4b7eb1a7a9e57650b02cde4
seoeunbae <enyoupark@gmail.com>
2023-04-29 21:25:23 +0900

    feat : implement following-entity

--- src/main/java/com/example/multithreadcodesample/domain/following/model/entity/Following.java
+++ src/main/java/com/example/multithreadcodesample/domain/following/model/entity/Following.java
@@ -1,7 +1,25 @@
 package com.example.multithreadcodesample.domain.following.model.entity;
 
 import com.example.multithreadcodesample.common.base.BaseEntity;
+import com.example.multithreadcodesample.domain.user.model.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
 public class Following extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_user_id")
+    private User followingUser;
 
 }
