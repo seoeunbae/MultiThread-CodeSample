4d1ac5f3ad89331862e0ae653124fd3f02c2a37e
seoeunbae <enyoupark@gmail.com>
2023-04-29 21:06:53 +0900

    feat : implement baseEntity

--- src/main/java/com/example/multithreadcodesample/common/base/BaseEntity.java
+++ src/main/java/com/example/multithreadcodesample/common/base/BaseEntity.java
@@ -1,4 +1,39 @@
 package com.example.multithreadcodesample.common.base;
 
-public class BaseEntity {
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "is_enabled")
+    private Boolean isEnabled = true;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || Hibernate.getClass(o) != Hibernate.getClass(this)) return false;
+        BaseEntity entity = (BaseEntity) o;
+
+        return entity.id == this.id && Objects.equals(createdAt, entity.createdAt);
+
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id,createdAt);
+    }
 }
