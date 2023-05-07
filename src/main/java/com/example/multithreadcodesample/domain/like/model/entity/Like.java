0ab96a358813b3824d875c0820433366cb64129a
seoeunbae <enyoupark@gmail.com>
2023-05-07 16:59:10 +0900

    feat : LikeService 완료

--- src/main/java/com/example/multithreadcodesample/domain/like/model/entity/Like.java
+++ src/main/java/com/example/multithreadcodesample/domain/like/model/entity/Like.java
@@ -3,6 +3,8 @@ package com.example.multithreadcodesample.domain.like.model.entity;
 import com.example.multithreadcodesample.common.base.BaseEntity;
 import com.example.multithreadcodesample.domain.user.model.entity.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import net.bytebuddy.dynamic.TargetType;
 
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "likes")
+@NoArgsConstructor
 public class Like extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -23,8 +26,16 @@ public class Like extends BaseEntity {
     @Column(name = "target_type")
     private TargetType targetType;
 
+    public Like(User user, long targetId, TargetType targetType) {
+        this.user = user;
+        this.targetId = targetId;
+        this.targetType = targetType;
+    }
+
     public enum TargetType{
         ASSET, STYLING, BRAND
     }
+
+
 
 }
--- src/main/java/com/example/multithreadcodesample/domain/like/service/LikeService.java
+++ src/main/java/com/example/multithreadcodesample/domain/like/service/LikeService.java
@@ -1,5 +1,6 @@
 package com.example.multithreadcodesample.domain.like.service;
 
+import com.example.multithreadcodesample.domain.like.listener.LikeEventListener;
 import com.example.multithreadcodesample.domain.like.model.entity.Like;
 import com.example.multithreadcodesample.domain.like.repository.LikeRepository;
 import com.example.multithreadcodesample.domain.user.model.entity.User;
@@ -9,19 +10,54 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeRepository likeRepository;
+    private final List<LikeEventListener> listeners;
+    private final LikeThreadPoolResolver threadPoolResolver;
+
     public void like(User user, long targetId, Like.TargetType targetType) {
         Optional<Like> optionalLike = likeRepository.findByUserIdAndTargetIdAndTargetType(user.getId(), targetId, targetType);
 
+        if(optionalLike.isPresent()){
+            final Like like = optionalLike.get();
+
+            if(!like.getIsEnabled()){
+                like.setIsEnabled(true);
+                likeRepository.save(like);
+
+                CompletableFuture.runAsync(
+                        () -> listeners.forEach(listener -> listener.onLike(like)),
+                        threadPoolResolver.get(targetType)
+                );
+            }
+        } else {
+            Like like =likeRepository.save(new Like(user, targetId, targetType));
+            CompletableFuture.runAsync(
+                    () -> listeners.forEach(listener -> listener.onLike(like)),
+                    threadPoolResolver.get(targetType)
+            );
+        }
 
     }
 
     public void unlike(User user, long targetId, Like.TargetType targetType) {
+        likeRepository.findByUserIdAndTargetIdAndTargetType(user.getId(), targetId, targetType)
+                .ifPresent(like -> {
+                    if (like.getIsEnabled()) {
+                        like.setIsEnabled(false);
+                        likeRepository.save(like);
 
+                        CompletableFuture.runAsync(
+                                () -> listeners.forEach(listener -> listener.onUnLike(user.getId(), targetId, targetType)),
+                                threadPoolResolver.get(targetType)
+                        );
+                    }
+                });
     }
 }
